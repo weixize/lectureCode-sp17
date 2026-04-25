14cd9d649daca48c9a20cfe97bc28fa175d2068f
Xize <weixize375@gmail.com>
2026-04-25 20:42:21 +0800

    done with lists2

--- lists2/DIY/fixAddLast/SLList.java
+++ lists2/DIY/fixAddLast/SLList.java
@@ -1,5 +1,5 @@
 public class SLList {
-    public class IntNode {
+    private static class IntNode {
         public int item;
         public IntNode next;
         public IntNode(int i, IntNode n) {
@@ -8,28 +8,29 @@ public class SLList {
         }
     }
 
-    private IntNode first; 
+    private IntNode sentinel;
     private int size;
 
     public SLList() {
-        first = null;
+        sentinel = new IntNode(63, null);
         size = 0;
     }
 
     public SLList(int x) {
-        first = new IntNode(x, null);
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
         size = 1;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
         size += 1;
     }    
 
     /** Retrieves the front item from the list. */
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
 
     /** Returns the number of items in the list. */
@@ -39,7 +40,8 @@ public class SLList {
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
-        IntNode p = first;
+        size += 1;
+        IntNode p = sentinel;
 
         /* Advance p to the end of the list. */
         while (p.next != null) {
